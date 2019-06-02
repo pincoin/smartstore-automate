c03a651ed72eb81518d60efd213c51f16018f6c7
pincoin <pincoins@gmail.com>
2019-06-02 19:13:40 +0900

    - logger

--- smartstore.py
+++ smartstore.py
@@ -1,5 +1,6 @@
 import glob
 import json
+import logging
 import os
 import time
 
@@ -11,7 +12,11 @@ from selenium.common.exceptions import NoSuchElementException
 
 from secret import *
 
-if __name__ == '__main__':
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('smartstore')
+
+
+def chrome_setup():
     options = webdriver.ChromeOptions()
     options.add_argument('user-data-dir={}'.format(chrome_profile_path))
     options.add_argument('profile-directory=Default')
@@ -42,6 +47,12 @@ if __name__ == '__main__':
             }
         })
 
+    return driver
+
+
+if __name__ == '__main__':
+    driver = chrome_setup()
+
     driver.implicitly_wait(3)
 
     driver.get('https://nid.naver.com/nidlogin.login')
@@ -52,35 +63,35 @@ if __name__ == '__main__':
     time.sleep(1)
 
     driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
-    print('로그인 완료')
+    logger.info('로그인 완료')
 
     try:
         driver.find_element_by_class_name('btn_cancel').click()
-        print('새로운 기기 등록 안함')
+        logger.info('새로운 기기 등록 안함')
         time.sleep(2)
     except NoSuchElementException:
         pass
 
     driver.get('https://sell.smartstore.naver.com/#/naverpay/sale/delivery?summaryInfoType=DELIVERY_READY')
-    print('스마트스토어 이동')
+    logger.info('스마트스토어 이동')
     time.sleep(3)
 
     driver.switch_to.frame(driver.find_element_by_id('__naverpay'))
-    print('iframe 선택')
+    logger.info('iframe 선택')
     time.sleep(3)
 
     driver.find_element_by_xpath('//select[@name="orderStatus"]/option[text()="발송대기"]').click()
-    print('발송대기 선택')
+    logger.info('발송대기 선택')
     time.sleep(3)
 
     # 엑셀 다운로드
     driver.find_element_by_class_name('_excelDownloadBtn').click()
-    print('엑셀 다운로드')
+    logger.info('엑셀 다운로드')
     time.sleep(5)
 
     # 엑셀 열기
     files = [f for f in glob.glob(os.path.join('{}/*.xlsx'.format(download_path))) if os.path.isfile(f)]
-    print(files)
+    logger.info(files)
 
     if len(files) == 1 and files[0].endswith('.xlsx'):
         wb_order = openpyxl.load_workbook(files[0])
@@ -114,7 +125,7 @@ if __name__ == '__main__':
                     'Cache-Control': 'no-cache',
                     'Authorization': 'Token {}'.format(api_token),
                 }
-                print(payload)
+                logger.info(payload)
                 requests.post(api_url, data=json.dumps(payload), headers=headers)
 
                 # 상품주문번호, 배송방법(배송없음), 택배사(공란), 송장번호(공란)
@@ -125,13 +136,13 @@ if __name__ == '__main__':
 
         if order_count > 0:
             wb_batch.save(os.path.join(download_path, batch_excel))
-            print('일괄발송 엑셀 파일 저장')
+            logger.info('일괄발송 엑셀 파일 저장')
 
             parent = driver.current_window_handle
             child = None
 
             driver.find_element_by_xpath('//button[text()="엑셀 일괄발송"]').click()
-            print('일괄발송 엑셀 팝업 띄우기')
+            logger.info('일괄발송 엑셀 팝업 띄우기')
             time.sleep(3)
 
             for handle in driver.window_handles:
@@ -143,25 +154,25 @@ if __name__ == '__main__':
 
             driver.find_element_by_xpath('//input[@name="uploadedFile"]') \
                 .send_keys(os.path.join(download_path, batch_excel))
-            print('일괄발송 엑셀 파일 선택')
+            logger.info('일괄발송 엑셀 파일 선택')
             time.sleep(5)
 
             driver.find_element_by_xpath('//span[text()="일괄 발송처리"]').click()
-            print('일괄발송 엑셀 파일 업로드')
+            logger.info('일괄발송 엑셀 파일 업로드')
             time.sleep(3)
 
             driver.find_element_by_xpath('//span[text()="닫기"]').click()
-            print('일괄발송 엑셀 팝업 닫기')
+            logger.info('일괄발송 엑셀 팝업 닫기')
             time.sleep(2)
 
             driver.switch_to.window(parent)
             time.sleep(3)
 
             os.remove(os.path.join(download_path, batch_excel))
-            print('일괄발송 엑셀 로컬 삭제')
+            logger.info('일괄발송 엑셀 로컬 삭제')
 
         wb_order.close()
         os.remove(files[0])
-        print('엑셀 로컬 삭제')
+        logger.info('엑셀 로컬 삭제')
 
     driver.quit()
